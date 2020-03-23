b76cfda6ef69ec99f49af11172304893bd60a0e6
Kyle Mitard <mitardk@gmail.com>
2020-03-23 15:12:43 -0400

    modified the way names are encoded
    The blank space at the end of each name is now an extra dimension in the one-hot vectors (as opposed to being represented by empty vectors). In addition, the raw names are capitalized, which I had mistakenly not accounted for, causing the first letter of every name to be percieved as a space.

--- training/encodeNames.py
+++ training/encodeNames.py
@@ -7,10 +7,15 @@
 	Takes three lists of names and encodes them into one-hot vectors so they can be used for machine learning stuffs
 '''
 
+# Encodes a text file (located at names_raw/title.txt) of names with every name on a new line into a group of one-hot vectors. The encoded names
+# are put in another text file at names_encoded/title.txt, so they can be imported into Octave/MATLAB
+# 
+# Param title = a string containing the name of the file
 def encodeList(title):
 
 	# indeces of this string are used when creating vectors
-	ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+	# space is to represent the empty space after the end, since they are all padded out to the same length
+	ALPHABET = 'abcdefghijklmnopqrstuvwxyz '
 
 	# open the files containing the raw names and the encoded names
 	file_raw = open('names_raw/' + title + '.txt', 'r').read().split('\n')
@@ -23,17 +28,21 @@ def encodeList(title):
 			maxLen = len(n)
 
 	for name_raw in file_raw:
+		
+		# initialize encoded name as empty
 		name_encoded = ''
+		
+		# the names in the list are capitalized, so I must account for that
+		name_raw = name_raw.lower()
 
-		# pad out the raw name with spaces to normalize length
+		# pad out the raw name with spaces as needed to normalize length
 		name_raw += ' ' * (maxLen - len(name_raw))
 		
 		for letter in name_raw:
 			letterIndex = ALPHABET.find(letter)
-			if letterIndex != -1:
-				name_encoded += '0 ' * letterIndex
-				name_encoded += '1 '
-			name_encoded += '0 ' * (25 - letterIndex)
+			name_encoded += '0 ' * letterIndex
+			name_encoded += '1 '
+			name_encoded += '0 ' * (26 - letterIndex)
 
 		file_encoded.write(name_encoded + '\n')
 
